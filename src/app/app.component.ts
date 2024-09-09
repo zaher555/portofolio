fc0a381711e0234a77293f5ae8c5b90db99fb83e
zaher555 <zahermohmed555@gmail.com>
2024-09-10 02:01:25 +0300

    last updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,12 @@ import { AboutComponent } from "./components/about/about.component";
 import { SkillsComponent } from "./components/skills/skills.component";
 import { ContactComponent } from "./components/contact/contact.component";
 import { FooterComponent } from "./components/footer/footer.component";
+import { ProjectsComponent } from "./components/projects/projects.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, LandingComponent, AboutComponent, SkillsComponent, ContactComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, LandingComponent, AboutComponent, SkillsComponent, ContactComponent, FooterComponent, ProjectsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
